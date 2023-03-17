16d50822ed53813fccb32946a292300fb18db8f5
Diego Aguado <diego.aguado@bosonit.com>
2023-03-17 09:43:04 +0100

    Small changes

--- block1-process-file-and-streams/src/main/java/org/example/Main.java
+++ block1-process-file-and-streams/src/main/java/org/example/Main.java
@@ -14,16 +14,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String path = sc.next();
-
-        List<Person> list = personList(path);
+        List<Person> lista = personList(path);
+        for (Person p : lista) System.out.println(p);
+        //filterNames(path);
         //filterNames(list);
         //list = filterAges(list);
         //System.out.println(filterMadrid(list));
         //System.out.println(filterBarcelona(list));
-        for (Person p : list) System.out.println(p);
+        //for (Person p : filterAges(path)) System.out.println(p);
     }
 
-    //Método que pasada una ruta de un fichero, lee el fichero y devuelve una lista de personas
+    // With the file path as parameter, this method reads the file and returns a list of people
     public static List<Person> personList(String path) throws InvalidLineFormatException {
         List<Person> l = new ArrayList();
         String name = "", town = "", line;
@@ -39,79 +40,80 @@ public class Main {
                 if (line.indexOf(":") == -1)
                     throw new InvalidLineFormatException("Format is not correct in line " + i + ". It should be -> name:town:age.");
 
-                name = line.substring(0, line.indexOf(":")); //Obtiene el nombre
+                name = line.substring(0, line.indexOf(":")); //Obtains name
 
                 if (name == "" || name == " ")
                     throw new InvalidLineFormatException("Format is not correct in line " + i + ". The name is necessary.");
 
-                line = line.substring(line.indexOf(":") + 1); //Si el nombre no está vacío lo corta
+                line = line.substring(line.indexOf(":") + 1); //Cuts the name
                 if (line.indexOf(":") != -1) {
-                    town = line.substring(0, line.indexOf(":")); //Si hay más campos obtiene la ciudad
-                    line = line.substring(line.indexOf(":") + 1); //Corta la ciudad
-                    age = (line == "" || line == " ") ? 0 : Integer.parseInt(line);
+                    town = line.substring(0, line.indexOf(":")); //If there is more data in the line it gets the town
+                    line = line.substring(line.indexOf(":") + 1); //Cuts the town
+                    age = (line == "" || line == " ") ? 0 : Integer.parseInt(line); //If there is a blank space age is 0 but if not, it gets the age
                 } else {
-                    town = line; //Si no da valor a la ciudad
-                    age = 0; //Y la edad se queda en 0
+                    town = line; //If there isn't more data it gets the town
+                    age = 0; //and age is 0
                 }
 
-                l.add(new Person(name, town, age)); //Añade la persona a la lista
+                l.add(new Person(name, town, age)); //Adds a person with the previous attributes to the list
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return l; //Devuelve la lista de personas del fichero
+        return l; //Returns the person list from the file
     }
 
-    //Método que devuelve una lista pasada por parámetro pero las ciudades vacías pasan a contener 'unknown'
+    //This method returns the same list from the parameter but changes empty towns to 'unknown'
     public static List<Person> unknownTown(List<Person> list) {
         for (Person p : list) {
             if (p.getTown() == "" || p.getTown() == " ")
-                p.setTown("unknown"); //Si la ciudad está vacía la cambia a 'unknown'
+                p.setTown("unknown"); //If the town is empty, changes it to 'unknown'
         }
 
-        return list; //Devuelve la misma lista pero sin ciudades vacías
+        return list; //Returns the same list but now with no empty towns
     }
 
-    //Método que dada una lista, devuelve otra con personas con edad introducida y menores de 25 años
-    public static List<Person> filterAges(List<Person> list) {
-        return unknownTown(list.stream().filter(u -> u.getAge() < 25 && u.getAge() != 0).toList());
-        //Convierte la List a Stream, filtra por la edad y lo convierte a List otra vez y se la pasa a unknownTown() para cambiar las ciudades vacías a 'unknown' y la devuelve
+    //This method returns a list of people with less than 25 years old
+    public static List<Person> filterAges(String path) {
+        return unknownTown(personList(path).stream().filter(u -> u.getAge() < 25 && u.getAge() != 0).toList());
+        //Converts a List to a Stream, filters by age, converts it again to a List and then uses method unknownTown() to change empty towns to 'unknown' and then returns it
     }
 
-    //Método que dada una lista, la filtra eliminando aquellas personas que su nombre empieza por 'A' y escribe la lista por pantalla
-    public static void filterNames(List<Person> list) {
-        List<Person> newList = list.stream().filter(u -> u.getName().indexOf("A") == 0).toList();
-        //Convierte la List a Stream, filtra por nombres que empiecen por 'A', lo convierte a List de nuevo y lo almacena en una lista auxiliar
+    //This method filters a list, deleting people whose name starts with an 'A' and then prints it on screen
+    public static void filterNames(String path) {
+        List<Person> list = personList(path);
+        List<Person> newList = personList(path).stream().filter(u -> u.getName().indexOf("A") == 0).toList();
+        //Converts a List to a Stream, filters by names, converts it again to a List and then saves the list in an auxiliar list
 
-        for (Person p : newList) { //Recorre la lista auxiliar
-            for (Person e : list) { //Recorre la lista original
-                if (e.equals(p)) { //Compara las personas de la original con las de la auxiliar
-                    list.remove(p); //Si coincide se borra de la lista
+        for (Person p : newList) { //Travels the auxilar list
+            for (Person e : list) { //Travels the original list
+                if (e.equals(p)) { //Compares people from the auxiliar list with people from the original list
+                    list.remove(p); //If it's the same person, it gets deleted
                     break;
                 }
             }
         }
 
-        list = unknownTown(list); //Lista
+        list = unknownTown(list);
 
-        for (Person p : list) System.out.println(p); //Recorre la lista e imprime las personas
+        for (Person p : list) System.out.println(p); //Travels the list and prints people on screen
     }
 
-    //Método que filtra la lista de personas por edad (<25) y luego por la ciudad de Madrid y devuelve la primera persona
-    public static Person filterMadrid(List<Person> list) {
-        Stream<Person> l = filterAges(list).stream(); //Filtra primero por la edad
-        Optional<Person> s = l.filter(u -> u.getTown().equals("Madrid")).findFirst(); //De la lista restante filtra por la ciudad Madrid y se queda con el primero
+    //This method filters the list by age and then by town, where it must be 'Madrid', and then prints the first person
+    public static Person filterMadrid(String path) {
+        Stream<Person> l = filterAges(path).stream(); //Filters list by age
+        Optional<Person> s = l.filter(u -> u.getTown().equals("Madrid")).findFirst(); //Then filters by town and takes the first person
 
-        if (s.isPresent()) return s.get(); //Si hay personas de Madrid devuelve la persona
-        else return null; //Sino devuelve null
+        if (s.isPresent()) return s.get(); //If there is, at least, 1 person  from Madrid, it printes it
+        else return null; //If not, it prints null
     }
 
-    //Método que filtra la lista de personas por edad (<25) y luego por la ciudad de Barcelona y devuelve la primera persona
-    public static Person filterBarcelona(List<Person> list) {
-        Stream<Person> l = filterAges(list).stream(); //Filtra primero por la edad
-        Optional<Person> s = l.filter(u -> u.getTown().equals("Barcelona")).findFirst(); //De la lista restante filtra por la ciudad Barcelona y se queda con el primero
+    //This method filters by age and then by town, where it must be 'Barcelona', and then prints the first person
+    public static Person filterBarcelona(String path) {
+        Stream<Person> l = filterAges(path).stream(); //Filters list by age
+        Optional<Person> s = l.filter(u -> u.getTown().equals("Barcelona")).findFirst(); //Then filters by town and takes the first person
 
-        if (s.isPresent()) return s.get(); //Si hay personas de Barcelona devuelve la persona
-        else return null; //Sino devuelve null
+        if (s.isPresent()) return s.get(); //If there is, at least, 1 person  from Barcelona, it printes it
+        else return null; //If not, it prints null
     }
 }
